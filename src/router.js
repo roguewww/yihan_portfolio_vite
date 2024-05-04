7200103ba856ee1d4db3c610d77d561ac1777540
roguewww <rogue.c3c@gmail.com>
2024-05-04 11:14:16 +0800

    Added realrogue mainpage

--- src/router.js
+++ src/router.js
@@ -1,11 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from './components/HomePage.vue';  // Your main or home page component
-import DrawPage from './components/DrawPage.vue';  // Your drawing component
+import HomePage from './components/HomePage.vue'; 
+import DrawPage from './components/DrawPage.vue';  
 import Test from './components/TestPage.vue';
+import RealRogue from './components/RealRogue.vue';
+
 const routes = [
   { path: '/homepage', name: 'Home', component: HomePage },
   { path: '/draw/:x_Pos/:y_Pos', name: 'Draw', component: DrawPage, props: true },
-  { path: '/test', name: 'Test', component: Test}
+  { path: '/test', name: 'Test', component: Test},
+  { path: '/rogue', name: 'realrogue', component: RealRogue}
 ];
 
 const router = createRouter({
